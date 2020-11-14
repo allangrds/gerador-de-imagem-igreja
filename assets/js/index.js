f55c7b4a34f227d103a4b4fd03081f7565812873
Allan Ramos <allangrds@gmail.com>
2020-11-14 17:37:32 -0300

    feat(eslint): add lib

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,67 +1,67 @@
-import domtoimage from 'dom-to-image';
+import domtoimage from 'dom-to-image'
 
 function getContentFromForm (form) {
-    let content = {};
+  const content = {}
 
-    for(let i = 0; i < form.elements.length; i++) {
-        const item = form.elements[i];
+  for (let i = 0; i < form.elements.length; i += 1) {
+    const item = form.elements[i]
 
-        if (item.value) {
-            content[item.name] = item.value
-        }
+    if (item.value) {
+      content[item.name] = item.value
     }
+  }
 
-    return content;
+  return content
 }
 
 function setElementValues (content) {
-    if (content['message']) {
-        document.getElementsByClassName("message")[0].innerHTML = `"${content['message']}"`;
-        document.getElementsByClassName("message")[0].style.display = "block";
-    } else {
-        document.getElementsByClassName("message")[0].style.display = "none";
-    }
+  if (content.message) {
+    document.getElementsByClassName('message')[0].innerHTML = `"${content.message}"`
+    document.getElementsByClassName('message')[0].style.display = 'block'
+  } else {
+    document.getElementsByClassName('message')[0].style.display = 'none'
+  }
 
-    document.getElementsByClassName("message-date")[0].innerHTML = `${content['message-date']}`;
-    document.getElementsByClassName("preacher")[0].innerHTML = `${content['preacher']}`;
-    document.getElementsByClassName("preacher-image")[0].src = `${content['preacher-image']}`;
-    document.getElementsByClassName("church-logo")[0].src = `${content['church-logo']}`;
-    document.getElementsByClassName("church-local")[0].src = `${content['church-local']}`;
+  document.getElementsByClassName('message-date')[0].innerHTML = `${content['message-date']}`
+  document.getElementsByClassName('preacher')[0].innerHTML = `${content.preacher}`
+  document.getElementsByClassName('preacher-image')[0].src = `${content['preacher-image']}`
+  document.getElementsByClassName('church-logo')[0].src = `${content['church-logo']}`
+  document.getElementsByClassName('church-local')[0].src = `${content['church-local']}`
 }
 
 function viewImage () {
-    const form = document.getElementById('form');
+  const form = document.getElementById('form')
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-        const content = getContentFromForm(form);
+    const content = getContentFromForm(form)
 
-        setElementValues(content);
-    });
+    setElementValues(content)
+  })
 }
 
 function saveImage () {
-    const button = document.getElementById('btn-save-image');
-    const div = document.getElementById('generated-image');
-    const options =  {
-        height: 550,
-        width: 500,
-    }
+  const button = document.getElementById('btn-save-image')
+  const div = document.getElementById('generated-image')
+  const options = {
+    height: 550,
+    width: 500,
+  }
 
-    button.addEventListener('click', function() {
-        domtoimage.toJpeg(div, options)
-            .then(function (dataUrl) {
-                const link = document.createElement('a');
-                link.download = 'my-image-name.jpeg';
-                link.href = dataUrl;
-                link.click();
-            })
-            .catch((err) => alert('Erro ao gerar a imagem'));
-    });
+  button.addEventListener('click', () => {
+    domtoimage.toJpeg(div, options)
+      .then((dataUrl) => {
+        const link = document.createElement('a')
+        link.download = 'my-image-name.jpeg'
+        link.href = dataUrl
+        link.click()
+      })
+      .catch(() => alert('Erro ao gerar a imagem'))
+  })
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-    viewImage();
-    saveImage();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  viewImage()
+  saveImage()
+})
