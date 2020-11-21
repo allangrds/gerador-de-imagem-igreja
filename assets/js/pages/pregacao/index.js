94e13d9c8c8d842232d941280cfb5e0a615f4593
Allan Ramos <allangrds@gmail.com>
2020-11-21 20:51:02 -0300

    feat(preacher-message): export image to whatsapp, instagram and facebook resolution

--- assets/js/pages/pregacao/index.js
+++ assets/js/pages/pregacao/index.js
@@ -23,25 +23,45 @@ function getContentFromForm (form) {
 function setElementValues (content) {
   if (content['preacher-message-title']) {
     document.getElementsByClassName('preacher-message-title')[0].innerHTML = `"${content['preacher-message-title']}"`
+    document.getElementsByClassName('preacher-message-title')[1].innerHTML = `"${content['preacher-message-title']}"`
+    document.getElementsByClassName('preacher-message-title')[2].innerHTML = `"${content['preacher-message-title']}"`
     document.getElementsByClassName('preacher-message-title')[0].style.display = 'block'
+    document.getElementsByClassName('preacher-message-title')[1].style.display = 'block'
+    document.getElementsByClassName('preacher-message-title')[2].style.display = 'block'
   } else {
     document.getElementsByClassName('preacher-message-title')[0].style.display = 'none'
+    document.getElementsByClassName('preacher-message-title')[1].style.display = 'none'
+    document.getElementsByClassName('preacher-message-title')[2].style.display = 'none'
   }
 
   document.getElementsByClassName('preacher-message-date')[0].innerHTML = content['preacher-message-date']
+  document.getElementsByClassName('preacher-message-date')[1].innerHTML = content['preacher-message-date']
+  document.getElementsByClassName('preacher-message-date')[2].innerHTML = content['preacher-message-date']
   document.getElementsByClassName('preacher-message-name')[0].innerHTML = content['preacher-name']
+  document.getElementsByClassName('preacher-message-name')[1].innerHTML = content['preacher-name']
+  document.getElementsByClassName('preacher-message-name')[2].innerHTML = content['preacher-name']
   document.getElementsByClassName('preacher-message-image')[0].src = content['preacher-image-url']
+  document.getElementsByClassName('preacher-message-image')[1].src = content['preacher-image-url']
+  document.getElementsByClassName('preacher-message-image')[2].src = content['preacher-image-url']
 
   if (content['church-logo-url']) {
     document.getElementsByClassName('preacher-message-church-logo')[0].src = `${content['church-logo-url']}`
+    document.getElementsByClassName('preacher-message-church-logo')[1].src = `${content['church-logo-url']}`
+    document.getElementsByClassName('preacher-message-church-logo')[2].src = `${content['church-logo-url']}`
   } else {
     document.getElementsByClassName('preacher-message-church-logo')[0].src = churchLogo
+    document.getElementsByClassName('preacher-message-church-logo')[1].src = churchLogo
+    document.getElementsByClassName('preacher-message-church-logo')[2].src = churchLogo
   }
 
   if (content['church-address']) {
     document.getElementsByClassName('preacher-message-church-address')[0].innerHTML = `${content['church-address']}`
+    document.getElementsByClassName('preacher-message-church-address')[1].innerHTML = `${content['church-address']}`
+    document.getElementsByClassName('preacher-message-church-address')[2].innerHTML = `${content['church-address']}`
   } else {
     document.getElementsByClassName('preacher-message-church-address')[0].innerHTML = 'Rua Martim Affonso, 152 - Belenzinho, São Paulo - SP, 03057-050'
+    document.getElementsByClassName('preacher-message-church-address')[1].innerHTML = 'Rua Martim Affonso, 152 - Belenzinho, São Paulo - SP, 03057-050'
+    document.getElementsByClassName('preacher-message-church-address')[2].innerHTML = 'Rua Martim Affonso, 152 - Belenzinho, São Paulo - SP, 03057-050'
   }
 }
 
@@ -70,28 +90,61 @@ function retrieveChurchAddress () {
   }
 }
 
-function saveImage () {
-  const button = document.getElementById('btn-save-image')
-  const div = document.getElementsByClassName('preacher-message-whatsapp')[0]
+function saveImage (type, options) {
+  const div = document.getElementsByClassName(`preacher-message ${type}`)[0]
+
+  domtoimage.toJpeg(div, options)
+    .then((dataUrl) => {
+      const form = document.getElementById('form')
+      const content = getContentFromForm(form)
+
+      saveChurchAddress(content['church-address'])
+
+      const link = document.createElement('a')
+      link.download = `pregacao-${type}.jpeg`
+      link.href = dataUrl
+      link.click()
+    })
+    .catch(() => alert('Erro ao gerar a imagem'))
+}
+
+function saveWhatsappImage () {
   const options = {
     height: 600,
     width: 400,
   }
+  const type = 'whatsapp'
+
+  saveImage(type, options)
+}
+
+function saveInstagramImage () {
+  const options = {
+    height: 1080,
+    width: 1080,
+  }
+  const type = 'instagram'
+
+  saveImage(type, options)
+}
+
+function saveFacebookImage () {
+  const options = {
+    height: 1200,
+    width: 1200,
+  }
+  const type = 'facebook'
+
+  saveImage(type, options)
+}
+
+function handleOnSave () {
+  const button = document.getElementById('btn-save-image')
 
   button.addEventListener('click', () => {
-    domtoimage.toJpeg(div, options)
-      .then((dataUrl) => {
-        const form = document.getElementById('form')
-        const content = getContentFromForm(form)
-
-        saveChurchAddress(content['church-address'])
-
-        const link = document.createElement('a')
-        link.download = 'my-image-name.jpeg'
-        link.href = dataUrl
-        link.click()
-      })
-      .catch(() => alert('Erro ao gerar a imagem'))
+    saveWhatsappImage()
+    saveInstagramImage()
+    saveFacebookImage()
   })
 }
 
@@ -101,7 +154,7 @@ function retrieveInputValues () {
 
 function initScript () {
   viewImage()
-  saveImage()
+  handleOnSave()
   retrieveInputValues()
 }
 
